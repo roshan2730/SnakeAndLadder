d364e1013cc7101edf7eef7be908fb61980f138a
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-05 14:47:39 +0530

    uc4:Added Repeat function till player reach win point

---
+++ SnakeLadder.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+public class SnakeLadder {
+    static Random rn=new Random();
+    final static int winPoint=100;
+    final static int startPosition=0;
+    final static int NumberOfPlayer=0;
+    final static int noPlay=1;
+    final static int ladder=2;
+    final static int snake=3;
+    static int userPosition=startPosition;
+
+    public static int getUserPosition() {
+        return userPosition;
+    }
+
+    public static void setUserPosition(int userPosition) {
+        SnakeLadder.userPosition = userPosition;
+    }
+
+    public static int rollDice()
+    {
+        int diceValue=rn.nextInt(6)+1;
+        return diceValue;
+    }
+
+    public static int playerOption()
+    {
+        int option=rn.nextInt(3)+1;
+        return option;
+    }
+    public static void main(String[] args) {
+
+        while ( getUserPosition() <= winPoint)
+        {
+            int numberOnDice=rollDice();
+            int option= playerOption();
+            if ( getUserPosition() < 1)
+            {
+                setUserPosition(startPosition);
+            }
+            if (option == noPlay)
+            {
+                setUserPosition(getUserPosition());
+            }
+            else if (option == ladder)
+            {
+                setUserPosition(getUserPosition()+rollDice());
+            }
+            else if(option == snake)
+            {
+                setUserPosition(getUserPosition()-rollDice());
+            }
+    			System.out.println(getUserPosition());      
+        }
+    }
+}
