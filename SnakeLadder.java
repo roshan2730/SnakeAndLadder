5fc344ccd527375f9e5d1adf0dd0ee18f353e3d9
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-05 14:34:32 +0530

    uc1:Added single player position

---
+++ SnakeLadder.java
@@ -0,0 +1,8 @@
+public class SnakeLadder {
+    final static int winPoint=100;
+    final static int startPosition=0;
+    final static int NumberOfPlayer=1;
+    public static void main(String[] args) {
+		System.out.println("This is uc1");
+	 }
+}
